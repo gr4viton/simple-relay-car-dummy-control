404c6ba966416abe1252dd53b8a7035454c6dd6c
gr4viton <gr4viton@users.noreply.github.com>
2023-02-24 10:00:06 +0100

    docs

--- arduinoDan.ino
+++ arduinoDan.ino
@@ -1,16 +1,9 @@
 /*
-  Fade
-
-  This example shows how to fade an LED on pin 9 using the analogWrite()
-  function.
-
-  The analogWrite() function uses PWM, so if you want to change the pin you're
-  using, be sure to use another PWM capable pin. On most Arduino, the PWM pins
-  are identified with a "~" sign, like ~3, ~5, ~6, ~9, ~10 and ~11.
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Fade
+  Code for a 3d printed car model which he wanted to bring to life.
+  - battery powered via relay
+  - RC control 
+    - switching on blinkers leds and mp3 module with car wrooming sounds
+    - blinkers with the mp3 module key openning sound
 */
 
 int blinker_leds = 9;         // blinkry
